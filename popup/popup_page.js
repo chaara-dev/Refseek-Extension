2444f64db521a9f8b78dc665c5671fa8342c704e
LostBrickPlacer <118847543+LostBrickPlacer@users.noreply.github.com>
2023-11-27 20:27:45 -0800

    Added Github link button.

--- popup/popup_page.js
+++ popup/popup_page.js
@@ -1,3 +1,9 @@
 // display logo icon
 let logoIcon = browser.runtime.getURL("icons/RefSeek_logo_asterisk.gif");
 document.getElementById("image_logo").src = logoIcon;
+
+//Close popup on button click
+// NOT WORKING - FIND FIX
+function close() {
+    window.close();
+};

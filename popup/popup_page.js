7ff929aad9a4938a2fdf7e01347d6cec6127ebd6
LostBrickPlacer <118847543+LostBrickPlacer@users.noreply.github.com>
2023-11-29 18:57:33 -0800

    Changed .innerHTML to .textContent for versionNum

--- popup/popup_page.js
+++ popup/popup_page.js
@@ -4,7 +4,7 @@ document.getElementById("image_logo").src = logoIcon;
 
 // display version number in corner
 let versionNum = browser.runtime.getManifest().version
-document.getElementById("version-text").innerHTML = versionNum
+document.getElementById("version-text").textContent = versionNum
 
 //Close popup on button click
 // NOT WORKING - FIND FIX

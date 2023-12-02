20d0fbcec6b5b89514a1b63a7e342945d8ed9a58
LostBrickPlacer <118847543+LostBrickPlacer@users.noreply.github.com>
2023-12-01 21:22:24 -0800

    Removed close() function.

--- popup/popup_page.js
+++ popup/popup_page.js
@@ -5,9 +5,3 @@ document.getElementById("image_logo").src = logoIcon;
 // display version number in corner
 let versionNum = browser.runtime.getManifest().version
 document.getElementById("version-text").textContent = versionNum
-
-//Close popup on button click
-// NOT WORKING - FIND FIX
-function close() {
-    window.close();
-};
